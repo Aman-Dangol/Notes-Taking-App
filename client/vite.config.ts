807807a1d573e9014c9210b482c377762eb5b987
Aman-Dangol <amandangol423@gmail.com>
2025-08-12 12:47:20 +0545

    feat(client): final commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_URL,
+        target: loadEnv(mode, process.cwd(), "").VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
@@ -20,4 +20,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src/"),
     },
   },
-});
+}));
